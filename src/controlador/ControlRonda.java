080bf3575d94b5d3b25a4777d29b0ec2591453f7
Sarahy Caicedo <sarahy.caicedo@correounivalle.edu.co>
2023-05-09 14:18:36 -0500

    Agrego la documentacion

--- src/controlador/ControlRonda.java
+++ src/controlador/ControlRonda.java
@@ -1,8 +1,22 @@
+/*
+ Autores:
+ Sarahy Gisselle Caicedo Betancourth
+ Kevin Andres Giron Villegas
+ <sarahy.caicedo@correounivalle.edu.co>
+ <kevin.giron@correounivalle.edu.co>
+ Fecha creación: 2023-05-04
+ Fecha última modificación: 2023-05-07
+*/
 package controlador;
 
 import modelo.Ronda;
 import vista.GUI;
 import vista.ImagenDado;
+
+/*
+*INTENCION: Tiene como proposito controlar y manejar la logica del juego para
+*  determinar cuantas ondas se han jugado y validar si se ha llegado al final
+*/
 
 public class ControlRonda  {
     private Ronda num;
@@ -10,8 +24,18 @@ public class ControlRonda  {
 
     public static int dado;
     private GUI vista;
+
+    /*
+    * Constructor de la clase
+    */
+
     public ControlRonda(){
     }
+
+    /*
+    * Recibe como parametro el numero actual de la ronda y crea una instancia de la
+    * clase ronda para verificar si se ha alcanzado el limite de 5 rondas.
+    */
 
     public int rondas(int ron){
         num = new Ronda(ron);
@@ -21,6 +45,12 @@ public class ControlRonda  {
             return num.getNum();
         }
     }
+
+    /*
+    * Recibe la cantidad de caras en una ronda y el numero de dado correspondiente,
+    * crea una instancia de la clase Ronda y llama a la funcion estadoFinal, determinando
+    * si se ha llegado al final del juego.
+    */
 
     public void validacion(int cantidad, int numero){
         num = new Ronda();
--- src/vista/Fondo.java
+++ src/vista/Fondo.java
@@ -7,20 +7,32 @@
  Fecha creación: 2023-05-01
  Fecha última modificación: 2023-05-06
 */
-/*
- INTENCION: Esta clase tiene como porposito establecer un fondo a traves del metodo paintComponent.
- */
+
 package vista;
 
 import javax.swing.*;
 import java.awt.*;
 
+/*
+ INTENCION: Esta clase tiene como porposito establecer un fondo a traves del metodo paintComponent.
+ */
+
 public class Fondo extends JPanel {
     private Image imagenFondo;
+
+    /*
+    * Constructor de la clase
+    */
 
     public Fondo(Image imagenFondo) {
         this.imagenFondo = imagenFondo;
     }
+
+    /*
+    * Este metodo es llamado automaticamente por el sistema grafico cada vez
+    * que se necesita pintar o volver  pintar el componente, verificando si hay una imagen
+    * de fondo, y de ser afirmativo, dibuja en el panel utilizando el objeto Graphics.
+    */
 
     @Override
     protected void paintComponent(Graphics g) {
